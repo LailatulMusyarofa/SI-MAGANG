bba94bc9c7bd6a204efcc400bceb2f90d7694694
LailatulMusyarofa <lailatulmusyarofa287@gmail.com>
2025-07-11 10:59:08 +0700

    Tambah fitur Status Administrasi pd Dashboard

---
+++ resources/views/pages/home/fitur/ringkasan.blade.php
@@ -0,0 +1,20 @@
+<div class="container mt-4">
+    <div class="row g-3">
+        @foreach($ringkasan as $index => $item)
+        @php
+            $colors = ['text-indigo', 'text-yellow', 'text-green'];
+            $color = $colors[$index % count($colors)];
+        @endphp
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card card-lg h-100" style="border-radius: 24px;">
+                <div class="card-body">
+                    <h1 class="text-center {{ $color }}">
+                        {{ $item['value'] }}@if($index === 2)%@endif
+                    </h1>
+                    <h3 class="text-muted text-center">{{ $item['label'] }}</h3>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -4,85 +4,12 @@
             <div class="row g-2 align-items-center">
                 <!-- Breadcrumb -->
                 <x-breadcrumb pageTitle="Beranda"></x-breadcrumb>
+                <!-- NOTIFIKASI -->
                 <div class="col-auto ms-auto d-flex align-items-center">
-                 <ul class="navbar-nav">
-                   <li class="nav-item dropdown"><!-- MODIFIKASI -->
-                   <a href="#" class="nav-link" data-bs-toggle="dropdown" aria-label="Notifications">
-
-                <!-- Icon Bell -->
-                <svg class="icon icon-tabler icon-tabler-bell" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M5 17h14" />
-                <path d="M17 17v-6a5 5 0 0 0 -10 0v6" />
-                <path d="M9 21h6" />
-                </svg>
-
-                <!-- Titik merah animasi -->
-                <span class="status-dot status-dot-animated bg-red"></span>
-            </a>
-
-  <!-- Dropdown isi notifikasi -->
-  <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow dropdown-menu-card"><!-- MODIFIKASI -->
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">Status Administrasi</h3>
-        <div class="ms-auto">
-          <span class="badge bg-green-lt text-green">1 Baru</span>
-        </div>
-      </div>
-      <div class="card-body p-0">
-        <div class="list-group list-group-flush list-group-hoverable">
-          <!-- Notifikasi 1 -->
-          <div class="list-group-item">
-            <div class="row align-items-center">
-              <div class="col-auto">
-                <span class="status-dot status-dot-animated bg-blue"></span>
-              </div>
-              <div class="col text-truncate">
-                <a href="#" class="text-body d-block fw-bold">Permohonan Magang Baru dari Universitas Brawijaya</a>
-                <div class="text-truncate mt-n1" style="color: #6c757d;">
-                 3 Permohonan Magang baru sedang menunggu persetujuan
+                    <ul class="navbar-nav">
+                        @include('pages.home.fitur.notifikasi')
+                    </ul>
                 </div>
-
-              </div>
-            </div>
-          </div>
-          <!-- Notifikasi 2 -->
-          <div class="list-group-item">
-            <div class="row align-items-center">
-              <div class="col-auto">
-                <span class="status-dot status-dot-animated bg-gray"></span>
-              </div>
-              <div class="col text-truncate">
-                <a href="#" class="text-body d-block">Dokumen Perlu Diverifikasi</a>
-                <div class="text-truncate mt-n1" style="color: #6c757d;">Ahmad Rizki - dokumen CV perlu diverifikasi ulang</div>
-              </div>
-            </div>
-          </div>
-          <!-- Notifikasi 3 -->
-          <div class="list-group-item">
-            <div class="row align-items-center">
-              <div class="col-auto">
-                <span class="status-dot status-dot-animated bg-green"></span>
-              </div>
-              <div class="col text-truncate">
-                <a href="#" class="text-body d-block">Magang selesai</a>
-                <div class="text-truncate mt-n1" style="color: #6c757d;">Sari Dewi telah menyelesaikan program magang</div>
-              </div>
-            </div>
-          </div>
-          <!-- Tambah notifikasi lain di sini jika dibutuhkan -->
-        </div>
-      </div>
-    </div>
-  </div>
-</li>
-
-    </ul>
-</div>
-
             </div>
         </div>
     </x-page-header>
@@ -98,6 +25,11 @@
                     </div>
                 </div>
             </div>
+            <!-- FITUR STATUS ADMINISTRASI -->
+            @include('pages.home.fitur.statusAdministrasi')
+
+            <!-- FITUR RINGKASAN  -->
+             @include('pages.home.fitur.ringkasan')
         </div>
     </div>
 </x-app-layout>
