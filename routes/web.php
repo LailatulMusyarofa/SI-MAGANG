0da40fd36ac973dfdadd504e3126d201e0716fc5
Nurul Eka <nuruleka@MacBook-Air-Nurul.local>
2025-07-15 15:22:26 +0700

    menambahkan daftar administrasi

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdministrasiController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Auth;
+
 
 
 Route::middleware('guest')->group(function () {
@@ -203,6 +205,10 @@ Route::post('password/reset', function (Illuminate\Http\Request $request) {
                 ? redirect()->route('login')->with('status', __($status))
                 : back()->withErrors(['email' => [($status)]]);
 })->name('password.update');
+
+
+// daftar administrator
+Route::get('/master_administrasi', [AdministrasiController::class, 'master'])->name('master_administrasi');
 
 
 // Logout route
